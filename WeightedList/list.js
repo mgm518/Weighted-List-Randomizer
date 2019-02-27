5bf72be37fd337ea5961053b86bbada906ab9350
Mark McGuire <mgm518@users.noreply.github.com>
2019-02-27 14:02:00 -0600

    Add files via upload
    first initial upload from hard drive

---
+++ WeightedList/list.js
@@ -0,0 +1,356 @@
+var importList = {
+    "list1": [{
+            "name": "Pistachio Dream",
+            "weight": 100,
+            "active": true,
+            "url": "res\\icons\\Costume_60120_JinM_col1.png",
+            "bonus": [{
+                "item": "",
+                "mult": 0
+            }]
+        }, {
+            "name": "Traitor",
+            "weight": 50,
+            "active": true,
+            "url": "res\\icons\\Costume_60054_JinM_col3.png",
+            "bonus": [{
+                "item": "Cool Groove",
+                "mult": -75
+            }, {
+                "item": "Feline Frisky",
+                "mult": -75
+            }, {
+                "item": "Regium Corvus Clip",
+                "mult": -75
+            }]
+        }, {
+            "name": "Temptation",
+            "weight": 75,
+            "active": true,
+            "url": "res\\icons\\Costume_60109_JinM_col1.png",
+            "bonus": [{
+                "item": "Feline Frisky",
+                "mult": 50
+            }, {
+                "item": "Thunder Horns",
+                "mult": 30
+            }, {
+                "item": "Cool Groove",
+                "mult": -50
+            }, {
+                "item": "Regium Corvus Clip",
+                "mult": -50
+            }]
+        }, {
+            "name": "Summer Splash",
+            "weight": 120,
+            "active": true,
+            "url": "res\\icons\\Costume_10009_KunN_col1.png",
+            "bonus": [{
+                "item": "Cool Groove",
+                "mult": 30
+            }, {
+                "item": "Resolve",
+                "mult": 75
+            }]
+        }, {
+            "name": "Summer Groove",
+            "weight": 120,
+            "active": true,
+            "url": "res\\icons\\Costume_70000_JinF_.png",
+            "bonus": [{
+                "item": "Cool Groove",
+                "mult": 30
+            }]
+        }, {
+            "name": "Regium Corvus",
+            "weight": 100,
+            "active": true,
+            "url": "res\\icons\\NCW_Costume_10039_col1_All.png",
+            "bonus": [{
+                "item": "Regium Corvus Clip",
+                "mult": 50
+            }, {
+                "item": "Cool Groove",
+                "mult": 30
+            }]
+        }, {
+            "name": "Noble of the Sea",
+            "weight": 90,
+            "active": false,
+            "url": "res\\icons\\Costume_65045_JinM_col1.png",
+            "bonus": [{
+                "item": "Regium Corvus Clip",
+                "mult": -50
+            }, {
+                "item": "Infernal Lord's Eyebrows",
+                "mult": 75
+            }]
+        }, {
+            "name": "Millenium",
+            "weight": 110,
+            "active": true,
+            "url": "res\\icons\\Costume_60073_JinM_col1.png",
+            "bonus": [{
+                "item": "",
+                "mult": 0
+            }]
+        }, {
+            "name": "Fated Bond",
+            "weight": 90,
+            "active": false,
+            "url": "res\\icons\\Costume_60132_JinF_col3.png",
+            "bonus": [{
+                "item": "Regium Corvus Clip",
+                "mult": -50
+            }]
+        }, {
+            "name": "Adventurer",
+            "weight": 100,
+            "active": true,
+            "url": "res\\icons\\Costume_60025_JinM_col2.png",
+            "bonus": [{
+                "item": "Regium Corvus Clip",
+                "mult": -50
+            }, {
+                "item": "Infernal Lord's Eyebrows",
+                "mult": 60
+            }]
+        }, {
+            "name": "Temptress",
+            "weight": 90,
+            "active": false,
+            "url": "res\\icons\\Costume_60076_JinF_col1.png",
+            "bonus": [{
+                "item": "Infernal Lord's Eyebrows",
+                "mult": 60
+            }]
+        }, {
+            "name": "Profane Shroud",
+            "weight": 100,
+            "active": true,
+            "url": "res\\icons\\Costume_60074_JinM_col1.png",
+            "bonus": [{
+                "item": "Infernal Lord's Eyebrows",
+                "mult": 200
+            }]
+        }, {
+            "name": "Spellcaster",
+            "weight": 50,
+            "active": false,
+            "url": "res\\icons\\Costume_70029_JinF_col1.png",
+            "bonus": [{
+                "item": "Bewitched Hat",
+                "mult": 50000
+            }]
+        }, {
+            "name": "Master Class",
+            "weight": 100,
+            "active": true,
+            "url": "res\\icons\\Costume_60116_JinF_col3.png",
+            "bonus": [{
+                "item": "Resolve",
+                "mult": 75
+            }]
+        }, {
+            "name": "Midnight Action Hero",
+            "weight": 100,
+            "active": true,
+            "url": "res\\icons\\Costume_60127_jinM_col2.png",
+            "bonus": [{
+                "item": "Feline Frisky",
+                "mult": 150
+            }]
+        }, {
+            "name": "Shadowless Defender",
+            "weight": 100,
+            "active": false,
+            "url": "res\\icons\\Costume_40011_JinF_col2.png",
+            "bonus": [{
+                "item": "Feline Frisky",
+                "mult": -25
+            }]
+        }, {
+            "name": "Hongmoon Duster",
+            "weight": 90,
+            "active": false,
+            "url": "res\\icons\\Costume_60053_JinM_col1.png",
+            "bonus": [{
+                "item": "",
+                "mult": 0
+            }]
+        }, {
+            "name": "Ghost in the Shell",
+            "weight": 90,
+            "active": false,
+            "url": "res\\icons\\Costume_60098_JinM_col1.png",
+            "bonus": [{
+                "item": "Infernal Lord's Eyebrows",
+                "mult": 200
+            }]
+        }, {
+            "name": "Calm Seas",
+            "weight": 100,
+            "active": true,
+            "url": "res\\icons\\Costume_10023_jinM_col1.png",
+            "bonus": [{
+                "item": "Resolve",
+                "mult": 75
+            }]
+        }, {
+            "name": "Alice",
+            "weight": 80,
+            "active": true,
+            "url": "res\\icons\\Costume_10084_JinM_col1.png",
+            "bonus": [{
+                "item": "Alice Ribbon",
+                "mult": 300
+            }, {
+                "item": "Resolve",
+                "mult": 75
+            }]
+        }, {
+            "name": "Lamentation",
+            "weight": 90,
+            "active": true,
+            "url": "res\\icons\\Costume_60089_JinM_col3.png",
+            "bonus": [{
+                "item": "Resolve",
+                "mult": 75
+            }]
+        }, {
+            "name": "Starling",
+            "weight": 100,
+            "active": true,
+            "url": "res\\icons\\Costume_65045_JinM_col2.png",
+            "bonus": [{
+                "item": "Regium Corvus Clip",
+                "mult": 75
+            }, {
+                "item": "Infernal Lord's Eyebrows",
+                "mult": -50
+            }]
+        }, {
+            "name": "Sealed Palace Raiment",
+            "weight": 90,
+            "active": false,
+            "url": "res\\icons\\Costume_60022_JinM_col1.png",
+            "bonus": [{
+                "item": "Alice Ribbon",
+                "mult": -50
+            }]
+        }, {
+            "name": "Secretkeeper",
+            "weight": 90,
+            "active": true,
+            "url": "res\\icons\\TWN_Costume_10081_JinF_col1.png",
+            "bonus": [{
+                "item": "Secretkeeper Headpiece",
+                "mult": 10000
+            }, {
+                "item": "Alice Ribbon",
+                "mult": -75
+            }, {
+                "item": "Feline Frisky",
+                "mult": -75
+            }]
+        }, {
+            "name": "Stratus Empire",
+            "weight": 90,
+            "active": true,
+            "url": "res\\icons\\Costume_65083_JinM_col1.png",
+            "bonus": [{
+                "item": "Secretkeeper Headpiece",
+                "mult": -50
+            }, {
+                "item": "Alice Ribbon",
+                "mult": 50
+            }, {
+                "item": "Resolve",
+                "mult": 75
+            }]
+        }, {
+            "name": "Oblivion",
+            "weight": 100,
+            "active": true,
+            "url": "res\\icons\\Costume_60142_JinM_col1.png",
+            "bonus": [{
+                "item": "Secretkeeper Headpiece",
+                "mult": -50
+            }, {
+                "item": "Alice Ribbon",
+                "mult": 50
+            }]
+        }, {
+            "name": "Aransu Apprentice",
+            "weight": 95,
+            "active": true,
+            "url": "res\\icons\\Costume_10003_JinF_col1.png",
+            "bonus": [{
+                "item": "Secretkeeper Headpiece",
+                "mult": -50
+            }, {
+                "item": "Resolve",
+                "mult": 75
+            }]
+        } 
+        //{ "name": "", "weight": 0, "active": true, "url": "res\\icons\\", "bonus": [{ "item": "", "mult": 0 }] } 
+    ],
+    "list2": [{
+            "name": "Regium Corvus Clip",
+            "active": false,
+            "url": "res\\icons\\NCW_Cash_Costume_Decoration_0002.png",
+            "weight": 50,
+            "base": 50
+        }, {
+            "name": "Thunder Horns",
+            "active": true,
+            "url": "res\\icons\\Attach_040063_JinM_col1.png",
+            "weight": 75,
+            "base": 75
+        }, {
+            "name": "Cool Groove",
+            "active": true,
+            "url": "res\\icons\\Attach_Cool_Groove.png",
+            "weight": 75,
+            "base": 75
+        }, {
+            "name": "Feline Frisky",
+            "active": true,
+            "url": "res\\icons\\Attach_040202_JinM_col1.png",
+            "weight": 50,
+            "base": 50
+        }, {
+            "name": "Bewitched Hat",
+            "active": false,
+            "url": "res\\icons\\Attach_040173_JinM_col1.png",
+            "weight": 5,
+            "base": 5
+        }, {
+            "name": "Infernal Lord's Eyebrows",
+            "active": false,
+            "url": "res\\icons\\Attach_040021_All.png",
+            "weight": 10,
+            "base": 10
+        }, {
+            "name": "Alice Ribbon",
+            "active": true,
+            "url": "res\\icons\\Attach_040266_JinF_col1.png",
+            "weight": 75,
+            "base": 75
+        }, {
+            "name": "Secretkeeper Headpiece",
+            "active": false,
+            "url": "res\\icons\\TWN_Attach_040265_JinF_col1.png",
+            "weight": 15,
+            "base": 15
+        }, {
+            "name": "Resolve",
+            "active": true,
+            "url": "res\\icons\\Attach_Hair_112_JinF_col1.png",
+            "weight": 33,
+            "base": 33
+        }
+        //{"name": "", "active": true, "url": "", "weight": 0, "base": 0 }
+    ]
+};
